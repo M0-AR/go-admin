9f59c0c8fd140d412e7f3f77d146a09a6a4c42ad
m0-ar <s176492@student.dtu.dk>
2021-12-11 22:14:39 +0100

    Get All users and create user with default password "1234"

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,11 @@ func Setup(app *fiber.App) {
 	app.Post("/api/login", controllers.Login)
 
 	app.Use(middlewares.IsAuthenticated)
+
 	app.Get("/api/user", controllers.User)
 	app.Post("/api/logout", controllers.Logout)
+
+	app.Get("/api/users", controllers.AllUsers)
+	app.Post("/api/users", controllers.CreateUser)
+
 }
