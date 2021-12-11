46e85cdabf1c1c740c1c24e66188f6b5effa346f
m0-ar <s176492@student.dtu.dk>
2021-12-11 22:04:38 +0100

    Add middlewares

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,10 +1,10 @@
 package controllers
 
 import (
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"go-admin/database"
 	"go-admin/models"
+	"go-admin/util"
 	"golang.org/x/crypto/bcrypt"
 	"strconv"
 	"time"
@@ -63,12 +63,7 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer:    strconv.Itoa(int(user.Id)),
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 1 day
-	})
-
-	token, err := claims.SignedString([]byte("mdsecret"))
+	token, err := util.GenerateJwt(strconv.Itoa(int(user.Id)))
 
 	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
@@ -88,29 +83,14 @@ func Login(c *fiber.Ctx) error {
 	})
 }
 
-type Claims struct {
-	jwt.StandardClaims
-}
-
 func User(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 
-	token, err := jwt.ParseWithClaims(cookie, &Claims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("mdsecret"), nil
-	})
-
-	if err != nil || !token.Valid {
-		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"message": "unauthenticated",
-		})
-	}
-
-	claims := token.Claims.(*Claims)
+	id, _ := util.ParseJwt(cookie)
 
 	var user models.User
 
-	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	database.DB.Where("id = ?", id).First(&user)
 
 	return c.JSON(user)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,14 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"go-admin/controllers"
+	"go-admin/middlewares"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+
+	app.Use(middlewares.IsAuthenticated)
 	app.Get("/api/user", controllers.User)
 	app.Post("/api/logout", controllers.Logout)
 }
